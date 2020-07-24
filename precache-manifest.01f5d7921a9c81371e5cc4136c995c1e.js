f679caaaa72418922b2f2becf7a2b874f35d3b27
u-uysal <ufuk.uysl5@gmail.com>
2020-07-24 12:54:53 +0200

    Updates

---
+++ precache-manifest.01f5d7921a9c81371e5cc4136c995c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c516e49f09c8b752ee48b2e733b5b633",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "abd72508fc43310c04ff",
+    "url": "/TodoList/static/css/main.b8e58bbf.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/TodoList/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd72508fc43310c04ff",
+    "url": "/TodoList/static/js/main.e0b14e13.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
